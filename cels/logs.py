4361950d71940ed7e129ebd9ea465eca0fb968bf
Andrés Sopeña <asopena@ehmm.org>
2024-10-26 16:23:19 +0200

    fix: don't change logging configuration of client code (#13)
    When Cels is used as a library, it shouldn't change the logging
    configuration of the importing code. This change fixes that as before
    Cels was changing `basicConfig` regardless of if it was used from
    command line or imported.

--- cels/logs.py
+++ cels/logs.py
@@ -1,12 +1,18 @@
 import logging
+
+from rich.console import Console
 from rich.logging import RichHandler
 
 
-log_format = "%(message)s"
-logging.basicConfig(
-    level="NOTSET",
-    format=log_format,
-    datefmt="",
-    handlers=[RichHandler(show_time=False, show_path=False)],
-)
+def configure_logging(level):
+    error_console = Console(stderr=True)
+    logging.basicConfig(
+        level=level,
+        format="%(message)s",
+        datefmt="",
+        handlers=[RichHandler(console=error_console, show_time=False, show_path=False)],
+    )
+
+
 log = logging.getLogger("cels")
+log.addHandler(logging.NullHandler())
